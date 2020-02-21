09e5f8969a7936980355c169a406906427bc7e18
Kite1717 <mustafafirat.yilmaz@ogr.deu.edu.tr>
2020-02-21 23:38:34 +0300

    Upload init files of test1.hello-world

---
+++ t1-hello-world/src/main/java/com/test1/t1helloworld/T1HelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.test1.t1helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T1HelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T1HelloWorldApplication.class, args);
+	}
+
+}
---
+++ t1-hello-world/src/test/java/com/test1/t1helloworld/T1HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test1.t1helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T1HelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
