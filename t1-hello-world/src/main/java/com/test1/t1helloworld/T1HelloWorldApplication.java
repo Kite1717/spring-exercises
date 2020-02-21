26d8753176d8d0676239a5a38073f74c9d3620a6
Kite1717 <mustafafirat.yilmaz@ogr.deu.edu.tr>
2020-02-22 00:53:42 +0300

    Add some comment line and SignUp api

--- t1-hello-world/src/main/java/com/test1/t1helloworld/T1HelloWorldApplication.java
+++ t1-hello-world/src/main/java/com/test1/t1helloworld/T1HelloWorldApplication.java
@@ -7,6 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class T1HelloWorldApplication {
 
 	public static void main(String[] args) {
+
+		//sonucu görmek için tarayıcıda tomcat suncunun bağlı olduğu adres konsolda yazıyor
+		//https://localhost:8080
+		// mesajı görmek istediğimiz  MAP ettiğimiz kısma gidilmelidir
+		//https://localhost:8080/message çıktı gözükür
 		SpringApplication.run(T1HelloWorldApplication.class, args);
 	}
 
