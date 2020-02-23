0c0de247a6fcdd686fce06620d404925b1a0e973
Kite1717 <mustafafirat.yilmaz@ogr.deu.edu.tr>
2020-02-23 20:28:27 +0300

    Renamed Application

--- spring-example/spring-elasticsearch-lombok/src/main/java/com/test2/AppElasticsearch.java
+++ spring-example/spring-elasticsearch-lombok/src/main/java/com/test2/AppElasticsearch.java
@@ -2,8 +2,11 @@ package com.test2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @SpringBootApplication
+//Elasticsearch ün repository sini kullancağımız için ekledik
+@EnableElasticsearchRepositories
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class , args);
